64d73ccf815f3e5c470603832b4731c0fb4ba985
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-03-14 20:21:56 +0100

    redesigned function to respond to slack post req

--- functions/slack.js
+++ functions/slack.js
@@ -1,22 +1,24 @@
 
 
 exports.default = async (event, callback) => {
-    try{
-        const {challenge} = event.body
-        callback(null, {
-            statusCode: 200,
-            body: {challenge}
-        })
-        return{
-            statusCode: 200,
-            body: JSON.stringify("challenge sent")
-        }
-    }catch(err){
-        console.log('err', err)
-        callback(err)
-        return{
-            statusCode: 500,
-            body: JSON.stringify(err)
+    if(event.httpMethod === 'POST'){
+        try{
+            const {challenge} = event.body
+            callback(null, {
+                statusCode: 200,
+                body: {challenge}
+            })
+            return{
+                statusCode: 200,
+                body: JSON.stringify("challenge sent")
+            }
+        }catch(err){
+            console.log('err', err)
+            callback(err)
+            return{
+                statusCode: 500,
+                body: JSON.stringify(err)
+            }
         }
     }
 }
