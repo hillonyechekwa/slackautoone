5fdc551f81badf555d0bf7e0c91061d97e7e3e8a
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-03-14 20:50:09 +0100

    re-wrote the whole thing

--- functions/slack.js
+++ functions/slack.js
@@ -2,26 +2,12 @@
 
 exports.default = async (event, callback) => {
     if(event.httpMethod === 'POST'){
-        try{
-            const {challenge} = event.body
             callback(null, {
-                statusCode: 200,
-                body: {
-                    challenge: challenge
-                }
+              statusCode: 200,
+              body: {
+                challenge: event.body.challenge
+              }
             })
-            return{
-                statusCode: 200,
-                body: JSON.stringify("challenge sent")
-            }
-        }catch(err){
-            console.log('err', err)
-            callback(err)
-            return{
-                statusCode: 500,
-                body: JSON.stringify(err)
-            }
-        }
     }
 }
 
--- functions/test.js
+++ functions/test.js
@@ -2,6 +2,14 @@
 
 exports.handler = async(event, callback) => {
   // console.log('callback', callback)
+  if(event.httpMethod === 'POST'){
+    callback(null, {
+      statusCode: 200,
+      body: {
+        challenge: event.body.challenge
+      }
+    })
+  }
   return{
     statusCode: 200,
     body: JSON.stringify("Hello World!")
