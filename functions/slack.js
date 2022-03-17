d8dabf30339155f141b674a545ce6e1d12c23f21
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-03-17 16:53:41 +0100

    let's try the main function now

--- functions/slack.js
+++ functions/slack.js
@@ -4,7 +4,7 @@ exports.default = async (event, callback) => {
     const challenge = event.body.challenge;
 
     if(event.httpMethod === 'POST'){
-            const challenge = JSON.parse(event.body)
+            const challenge = JSON.parse(event.body.challenge)
             callback(null, {
                 statusCode: 200,
                 body: challenge
